ccd0148c14a6912afb98d75ed7ce6ddb569e3e2d
lewisre <lewisre@PC1152>
2020-03-16 13:02:30 +0000

    add home module

---
+++ src/Service/Modules/HomeModule.cs
@@ -0,0 +1,32 @@
+﻿namespace Linn.Stores.Service.Modules
+{
+    using Linn.Stores.Service.Models;
+
+    using Nancy;
+    using Nancy.Responses;
+
+    public sealed class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            this.Get("/", args => new RedirectResponse("/inventory"));
+            this.Get("/inventory", _ => this.GetApp());
+            this.Get("/inventory/(.*)/create", _ => this.GetApp());
+
+            this.Get("/inventory/signin-oidc-client", _ => this.GetApp());
+            this.Get("/inventory/signin-oidc-silent", _ => this.SilentRenew());
+
+            this.Get(@"^(.*)$", _ => this.GetApp());
+        }
+
+        private object SilentRenew()
+        {
+            return this.Negotiate.WithView("silent-renew");
+        }
+
+        private object GetApp()
+        {
+            return this.Negotiate.WithModel(ApplicationSettings.Get()).WithView("Index");
+        }
+    }
+}
