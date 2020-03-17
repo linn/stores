4f25bba06a63f508e6b508850a856852cda35349
lewisre <lewisre@PC1152>
2020-03-17 13:52:14 +0000

    add facade service and resource

---
+++ src/Facade/Services/PartFacadeService.cs
@@ -0,0 +1,86 @@
+﻿namespace Linn.Stores.Facade.Services
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq.Expressions;
+
+    using Domain.LinnApps.Parts;
+
+    using Linn.Common.Facade;
+    using Linn.Stores.Resources;
+
+    using PagedList.Core;
+
+    public class PartFacadeService : IFacadeService<Part, int, PartResource, PartResource>
+    {
+        public IResult<Part> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResult<ResponseModel<Part>> GetById(int id, IEnumerable<string> privileges)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResult<IEnumerable<Part>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResult<ResponseModel<IEnumerable<Part>>> GetAll(IEnumerable<string> privileges)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResult<IEnumerable<Part>> Search(string searchTerm)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResult<ResponseModel<IEnumerable<Part>>> Search(string searchTerm, IEnumerable<string> privileges)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResult<IPagedList<Part>> GetAll(int pageNumber, int pageSize)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResult<ResponseModel<IPagedList<Part>>> GetAll(int pageNumber, int pageSize, IEnumerable<string> privileges)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResult<IPagedList<Part>> GetAll<TKeySort>(int pageNumber, int pageSize, Expression<Func<Part, TKeySort>> keySelectorForSort, bool asc)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResult<ResponseModel<IPagedList<Part>>> GetAll<TKeySort>(int pageNumber, int pageSize, Expression<Func<Part, TKeySort>> keySelectorForSort, bool asc, IEnumerable<string> privileges)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResult<Part> Add(PartResource resource)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResult<ResponseModel<Part>> Add(PartResource resource, IEnumerable<string> privileges)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResult<Part> Update(int id, PartResource updateResource)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResult<ResponseModel<Part>> Update(int id, PartResource updateResource, IEnumerable<string> privileges)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Resources/PartResource.cs
@@ -0,0 +1,37 @@
+﻿namespace Linn.Stores.Resources
+{
+    public class PartResource
+    {
+        public int Id { get; set; }
+
+        public string PartNumber { get; set; }
+
+        public string Description { get; set; }
+
+        public string RootProduct { get; set; }
+
+        public string StockControlled { get; set; }
+
+        public string SafetyCriticalPart { get; set; }
+
+        public string ProductAnalysisCode { get; set; }
+
+        public string ParetoCode { get; set; }
+
+        public string AccountingCompany { get; set; }
+
+        public string EmcCriticalPart { get; set; }
+
+        public string SingleSourcePart { get; set; }
+
+        public string PerformanceCriticalPart { get; set; }
+
+        public string SafetyDataDirectory { get; set; }
+
+        public string CccCriticalPart { get; set; }
+
+        public string PsuPart { get; set; }
+
+        public string SafetyCertificateExpirationDate { get; set; }
+    }
+}
