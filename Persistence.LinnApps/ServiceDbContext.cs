471b4e7504e4f6486c835440f5ef5670283f6f1a
lewisre <lewisre@PC1152>
2020-03-17 11:47:15 +0000

    merging

--- Persistence.LinnApps/ServiceDbContext.cs
+++ Persistence.LinnApps/ServiceDbContext.cs
@@ -1,8 +1,41 @@
 ﻿namespace Persistence.LinnApps
 {
+    using Domain.LinnApps.Parts;
+
     using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.Logging;
 
     public class ServiceDbContext : DbContext
     {
+        public static readonly LoggerFactory MyLoggerFactory =
+            new LoggerFactory(new[] { new Microsoft.Extensions.Logging.Debug.DebugLoggerProvider() });
+
+        public DbSet<Part> Parts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            this.BuildParts(builder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var host = ConfigurationManager.Configuration["DATABASE_HOST"];
+            var userId = ConfigurationManager.Configuration["DATABASE_USER_ID"];
+            var password = ConfigurationManager.Configuration["DATABASE_PASSWORD"];
+            var serviceId = ConfigurationManager.Configuration["DATABASE_NAME"];
+
+            var dataSource =
+                $"(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST={host})(PORT=1521))(CONNECT_DATA=(SERVICE_NAME={serviceId})(SERVER=dedicated)))";
+
+            optionsBuilder.UseOracle($"Data Source={dataSource};User Id={userId};Password={password};");
+            optionsBuilder.UseLoggerFactory(MyLoggerFactory);
+            optionsBuilder.EnableSensitiveDataLogging(true);
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        private void BuildParts(ModelBuilder builder)
+        {
+
+        }
     }
 }
