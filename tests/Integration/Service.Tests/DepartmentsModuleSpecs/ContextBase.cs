44ad70638a03dca563e91d22bc8db063c1a010ee
lewisre <lewisre@PC1152>
2020-03-20 14:56:52 +0000

    missing tests

---
+++ tests/Integration/Service.Tests/DepartmentsModuleSpecs/ContextBase.cs
@@ -0,0 +1,62 @@
+﻿namespace Linn.Stores.Service.Tests.DepartmentsModuleSpecs
+{
+    using System.Collections.Generic;
+    using System.Security.Claims;
+    using Linn.Common.Facade;
+    using Linn.Common.Persistence;
+    using Linn.Stores.Domain.LinnApps;
+    using Linn.Stores.Facade.ResourceBuilders;
+    using Linn.Stores.Facade.Services;
+    using Linn.Stores.Resources;
+    using Linn.Stores.Service.Modules;
+    using Linn.Stores.Service.ResponseProcessors;
+
+    using Nancy.Testing;
+
+    using NSubstitute;
+
+    using NUnit.Framework;
+
+    public class ContextBase : NancyContextBase
+    {
+        protected IDepartmentsService DepartmentsService { get; private set; }
+
+        protected IQueryRepository<Department> DepartmentRepository { get; private set; }
+
+
+        [SetUp]
+        public void EstablishContext()
+        {
+            this.DepartmentsService = Substitute
+                .For<IDepartmentsService>();
+
+            this.DepartmentRepository = Substitute
+                .For<IQueryRepository<Department>>();
+
+            var bootstrapper = new ConfigurableBootstrapper(
+                with =>
+                {
+                    with.Dependency(this.DepartmentsService);
+                    with.Dependency(this.DepartmentRepository);
+                    with.Dependency<IResourceBuilder<Department>>(new DepartmentResourceBuilder());
+                    with.Dependency<IResourceBuilder<IEnumerable<Department>>>(new DepartmentsResourceBuilder());
+                    with.Module<DepartmentsModule>();
+                    with.ResponseProcessor<DepartmentsResponseProcessor>();
+                    with.RequestStartup(
+                        (container, pipelines, context) =>
+                        {
+                            var claims = new List<Claim>
+                                                 {
+                                                         new Claim(ClaimTypes.Role, "employee"),
+                                                         new Claim(ClaimTypes.NameIdentifier, "test-user")
+                                                 };
+                            var user = new ClaimsIdentity(claims, "jwt");
+
+                            context.CurrentUser = new ClaimsPrincipal(user);
+                        });
+                });
+
+            this.Browser = new Browser(bootstrapper);
+        }
+    }
+}
---
+++ tests/Integration/Service.Tests/DepartmentsModuleSpecs/WhenGettingAll.cs
@@ -0,0 +1,68 @@
+﻿namespace Linn.Stores.Service.Tests.DepartmentsModuleSpecs
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using FluentAssertions;
+
+    using Linn.Common.Facade;
+    using Linn.Stores.Domain.LinnApps;
+    using Linn.Stores.Resources;
+
+    using Nancy;
+    using Nancy.Testing;
+
+    using NSubstitute;
+
+    using NUnit.Framework;
+
+    public class WhenGettingAll : ContextBase
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            var departmentA = new Department
+                               {
+                                   DepartmentCode = "A",
+                                   Description = "description A"
+                               };
+            var departmentB = new Department
+                               {
+                                   DepartmentCode = "B",
+                                   Description = "description B"
+                               };
+
+            this.DepartmentsService.GetOpenDepartments()
+                .Returns(new SuccessResult<IEnumerable<Department>>(new List<Department> { departmentA, departmentB }));
+
+
+            this.Response = this.Browser.Get(
+                "/inventory/departments",
+                with =>
+                    {
+                        with.Header("Accept", "application/json");
+                    }).Result;
+        }
+
+        [Test]
+        public void ShouldReturnOk()
+        {
+            this.Response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+
+        [Test]
+        public void ShouldCallService()
+        {
+            this.DepartmentsService.Received().GetOpenDepartments();
+        }
+
+        [Test]
+        public void ShouldReturnResource()
+        {
+            var resource = this.Response.Body.DeserializeJson<IEnumerable<DepartmentResource>>().ToList();
+            resource.Should().HaveCount(2);
+            resource.Should().Contain(a => a.DepartmentCode == "A");
+            resource.Should().Contain(a => a.DepartmentCode == "B");
+        }
+    }
+}
