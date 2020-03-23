36fef67f4f745f08318aea8a57b01ca1ea0af14f
lewisre <lewisre@PC1152>
2020-03-23 13:07:36 +0000

    forgot files

---
+++ src/Facade/ResourceBuilders/SupplierResourceBuilder.cs
@@ -0,0 +1,34 @@
+﻿namespace Linn.Stores.Facade.ResourceBuilders
+{
+    using System;
+    using System.Collections.Generic;
+
+    using Linn.Common.Facade;
+    using Linn.Common.Resources;
+    using Linn.Stores.Domain.LinnApps;
+    using Linn.Stores.Resources;
+
+    public class SupplierResourceBuilder : IResourceBuilder<Supplier>
+    {
+        public SupplierResource Build(Supplier supplier)
+        {
+            return new SupplierResource
+                       {
+                           Id = supplier.Id,
+                           Name = supplier.Name,
+                       };
+        }
+
+        public string GetLocation(Supplier supplier)
+        {
+            throw new NotImplementedException();
+        }
+
+        object IResourceBuilder<Supplier>.Build(Supplier supplier) => this.Build(supplier);
+
+        private IEnumerable<LinkResource> BuildLinks(Supplier supplier)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
