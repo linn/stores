80e1d4b7b575f279f20c87538f92b3aadce9afa5
lewisre <lewisre@PC1152>
2020-03-23 14:35:26 +0000

    units of measure lookup

---
+++ src/Facade/ResourceBuilders/UnitOfMeasureResourceBuilder.cs
@@ -0,0 +1,33 @@
+﻿namespace Linn.Stores.Facade.ResourceBuilders
+{
+    using System;
+    using System.Collections.Generic;
+
+    using Linn.Common.Facade;
+    using Linn.Common.Resources;
+    using Linn.Stores.Domain.LinnApps;
+    using Linn.Stores.Resources;
+
+    public class UnitOfMeasureResourceBuilder : IResourceBuilder<UnitOfMeasure>
+    {
+        public UnitOfMeasureResource Build(UnitOfMeasure unitOfMeasure)
+        {
+            return new UnitOfMeasureResource
+                       {
+                           Unit = unitOfMeasure.Unit,
+                       };
+        }
+
+        public string GetLocation(UnitOfMeasure unitOfMeasure)
+        {
+            throw new NotImplementedException();
+        }
+
+        object IResourceBuilder<UnitOfMeasure>.Build(UnitOfMeasure unitOfMeasure) => this.Build(unitOfMeasure);
+
+        private IEnumerable<LinkResource> BuildLinks(UnitOfMeasure unitOfMeasure)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Facade/ResourceBuilders/UnitsOfMeasureResourceBuilder.cs
@@ -0,0 +1,28 @@
+﻿namespace Linn.Stores.Facade.ResourceBuilders
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using Linn.Common.Facade;
+    using Linn.Stores.Domain.LinnApps;
+    using Linn.Stores.Resources;
+
+    public class UnitsOfMeasureResourceBuilder : IResourceBuilder<IEnumerable<UnitOfMeasure>>
+    {
+        private readonly UnitOfMeasureResourceBuilder unitOfMeasureResourceBuilder = new UnitOfMeasureResourceBuilder();
+
+        public IEnumerable<UnitOfMeasureResource> Build(IEnumerable<UnitOfMeasure> unitOfMeasures)
+        {
+            return unitOfMeasures
+                .Select(a => this.unitOfMeasureResourceBuilder.Build(a));
+        }
+
+        object IResourceBuilder<IEnumerable<UnitOfMeasure>>.Build(IEnumerable<UnitOfMeasure> unitOfMeasures) => this.Build(unitOfMeasures);
+
+        public string GetLocation(IEnumerable<UnitOfMeasure> unitOfMeasures)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
