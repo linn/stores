f6274492faa6a538bd785e2ab508d5ee2f27beae
lewisre <lewisre@PC1152>
2020-03-23 13:06:58 +0000

    suppliers look up

---
+++ tests/Integration/Service.Tests/SuppliersModuleSpecs/WhenGettingAll.cs
@@ -0,0 +1,68 @@
+﻿namespace Linn.Stores.Service.Tests.SuppliersModuleSpecs
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using FluentAssertions;
+
+    using Linn.Common.Facade;
+    using Linn.Stores.Domain.LinnApps;
+    using Linn.Stores.Resources;
+
+    using Nancy;
+    using Nancy.Testing;
+
+    using NSubstitute;
+
+    using NUnit.Framework;
+
+    public class WhenGettingAll : ContextBase
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            var supplierA = new Supplier
+            {
+                Id = 1,
+                Name = "Supplier A"
+            };
+            var supplierB = new Supplier
+            {
+                Id = 2,
+                Name = "Supplier B"
+            };
+
+            this.SuppliersService.GetSuppliers()
+                .Returns(new SuccessResult<IEnumerable<Supplier>>(new List<Supplier> { supplierA, supplierB }));
+
+
+            this.Response = this.Browser.Get(
+                "/inventory/suppliers",
+                with =>
+                {
+                    with.Header("Accept", "application/json");
+                }).Result;
+        }
+
+        [Test]
+        public void ShouldReturnOk()
+        {
+            this.Response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+
+        [Test]
+        public void ShouldCallService()
+        {
+            this.SuppliersService.Received().GetSuppliers();
+        }
+
+        [Test]
+        public void ShouldReturnResource()
+        {
+            var resource = this.Response.Body.DeserializeJson<IEnumerable<SupplierResource>>().ToList();
+            resource.Should().HaveCount(2);
+            resource.Should().Contain(a => a.Name == "Supplier A");
+            resource.Should().Contain(a => a.Name == "Supplier B");
+        }
+    }
+}
