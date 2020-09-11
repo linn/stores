04f84f972a83534ada37fee975b9c3b02b333f92
lewisre <lewisre@PC1152>
2020-09-11 16:16:57 +0100

    domain PartService unit tests

---
+++ tests/Unit/Domain.LinnApps.Tests/PartServiceTests/WhenAddingPartAndUserHasPrivileges.cs
@@ -0,0 +1,40 @@
+﻿namespace Linn.Stores.Domain.LinnApps.Tests.PartServiceTests
+{
+    using System.Collections.Generic;
+
+    using FluentAssertions;
+    using FluentAssertions.Extensions;
+
+    using Linn.Stores.Domain.LinnApps.Parts;
+    using Linn.Stores.Domain.LinnApps.Sos;
+
+    using NSubstitute;
+
+    using NUnit.Framework;
+
+    public class WhenAddingPartAndUserHasPrivileges : ContextBase
+    {
+        private Part part;
+
+        private Part result;
+
+        private List<string> privileges;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.part = new Part();
+           this.privileges = new List<string> { "part.admin" };
+
+           this.AuthService.HasPermissionFor(AuthorisedAction.PartAdmin, this.privileges).Returns(true);
+
+           this.result = this.Sut.CreatePart(this.part, this.privileges);
+        }
+
+        [Test]
+        public void ShouldReturnNewPart()
+        {
+            this.result.Should().BeOfType<Part>();
+        }
+    }
+}
---
+++ tests/Unit/Domain.LinnApps.Tests/PartServiceTests/WhenPhasingOutAndScrapOrConvertNotSpecified.cs
@@ -0,0 +1,40 @@
+﻿namespace Linn.Stores.Domain.LinnApps.Tests.PartServiceTests
+{
+    using System;
+    using System.Collections.Generic;
+
+    using FluentAssertions;
+
+    using Linn.Stores.Domain.LinnApps.Exceptions;
+    using Linn.Stores.Domain.LinnApps.Parts;
+
+    using NSubstitute;
+
+    using NUnit.Framework;
+
+    public class WhenPhasingOutAndScrapOrConvertNotSpecified : ContextBase
+    {
+        private Part from;
+
+        private Part to;
+
+        private List<string> privileges;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.from = new Part { DatePhasedOut = null };
+            this.to = new Part { DatePhasedOut = DateTime.Today, ReasonPhasedOut = "reason", ScrapOrConvert = null };
+            this.privileges = new List<string> { "part.admin" };
+            this.AuthService.HasPermissionFor(AuthorisedAction.PartAdmin, this.privileges).Returns(true);
+
+            this.Sut.UpdatePart(this.from, this.to, this.privileges);
+        }
+
+        [Test]
+        public void ShouldDefaultToConvert()
+        {
+            this.from.ScrapOrConvert.Should().Be("CONVERT");
+        }
+    }
+}
