2ca56cb6f05e969567cc21361d1c5e897c269ef7
lewisre <lewisre@PC1152>
2020-10-02 11:15:39 +0100

    add part liveness changing logic to domain service + test

--- src/Facade/Services/IPartLiveService.cs
+++ src/Facade/Services/IPartLiveService.cs
@@ -1,10 +1,14 @@
 ﻿namespace Linn.Stores.Facade.Services
 {
+    using System.Collections.Generic;
+
     using Linn.Common.Facade;
     using Linn.Stores.Domain.LinnApps.Parts;
 
     public interface IPartLiveService
     {
         IResult<PartLiveTest> CheckIfPartCanBeMadeLive(int id);
+
+        // IResult<PartLiveTest> MakePartLive(int id, int user, List<Priv>)
     }
 }
