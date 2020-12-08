b5471245c23776b43c08bdea39cad26c207f729f
Lewis Renfrew <lewis.renfrew@linn.co.uk>
2020-12-08 08:51:28 +0000

    make call to correct method in tests

--- tests/Unit/Domain.LinnApps.Tests/MechPartSourceServiceTests/WhenGettingCapacitanceLetterAndNumeralCode.cs
+++ tests/Unit/Domain.LinnApps.Tests/MechPartSourceServiceTests/WhenGettingCapacitanceLetterAndNumeralCode.cs
@@ -9,18 +9,18 @@
         [Test]
         public void WhenUnitIsMicro()
         {
-            this.Sut.GetRkmCode("u", 1m).Should().Be("1uF");
-            this.Sut.GetRkmCode("u", 4.7m).Should().Be("4u7F");
-            this.Sut.GetRkmCode("u", 470m).Should().Be("470uF");
+            this.Sut.GetCapacitanceLetterAndNumeralCode("u", 1m).Should().Be("1uF");
+            this.Sut.GetCapacitanceLetterAndNumeralCode("u", 4.7m).Should().Be("4u7F");
+            this.Sut.GetCapacitanceLetterAndNumeralCode("u", 470m).Should().Be("470uF");
         }
 
         [Test]
         public void WhenUnitIsNano()
         {
-            this.Sut.GetRkmCode("n", 150000m).Should().Be("150000nF");
-            this.Sut.GetRkmCode("n", 2.2m).Should().Be("2n2F");
-            this.Sut.GetRkmCode("n", 1m).Should().Be("1nF");
-            this.Sut.GetRkmCode("n", 100000m).Should().Be("100000nF");
+            this.Sut.GetCapacitanceLetterAndNumeralCode("n", 150000m).Should().Be("150000nF");
+            this.Sut.GetCapacitanceLetterAndNumeralCode("n", 2.2m).Should().Be("2n2F");
+            this.Sut.GetCapacitanceLetterAndNumeralCode("n", 1m).Should().Be("1nF");
+            this.Sut.GetCapacitanceLetterAndNumeralCode("n", 100000m).Should().Be("100000nF");
         }
 
         [Test]
