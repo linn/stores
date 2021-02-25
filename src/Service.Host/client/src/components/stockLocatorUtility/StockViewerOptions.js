64755261a53881a518685ee94afe00f49fddfa44
Lewis Renfrew <lewis.renfrew@linn.co.uk>
2021-02-25 14:12:27 +0000

    remove duplicate field

--- src/Service.Host/client/src/components/stockLocatorUtility/StockViewerOptions.js
+++ src/Service.Host/client/src/components/stockLocatorUtility/StockViewerOptions.js
@@ -97,30 +97,6 @@ function StockViewerOptions({
                     />
                 </Grid>
                 <Grid item xs={2} />
-
-                <Grid item xs={3}>
-                    <Typeahead
-                        items={storageLocations}
-                        fetchItems={searchStorageLocations}
-                        modal
-                        links={false}
-                        clearSearch={clearStorageLocationsSearch}
-                        loading={storageLocationsLoading}
-                        label="Storage Location"
-                        title="Search Storage Locations"
-                        value={options.storageLocation}
-                        onSelect={newValue =>
-                            setOptions({
-                                ...options,
-                                storageLocation: newValue.locationCode,
-                                locationId: newValue.id
-                            })
-                        }
-                        history={history}
-                        debounce={1000}
-                        minimumSearchTermLength={2}
-                    />
-                </Grid>
                 <Grid item xs={3}>
                     <InputField
                         label="Pallet Number"
