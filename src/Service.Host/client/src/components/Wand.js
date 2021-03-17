9d1240c3860c739c5b7b212308caa8503d89c102
Richard Phillips <richardiphillips@users.noreply.github.com>
2021-03-17 14:47:38 +0000

    refocus (#119)

--- src/Service.Host/client/src/components/Wand.js
+++ src/Service.Host/client/src/components/Wand.js
@@ -56,6 +56,9 @@ function Wand({
 
     useEffect(() => {
         setWandString(null);
+        if (wandStringInput.current) {
+            wandStringInput.current.focus();
+        }
     }, [items]);
 
     useEffect(() => {
