7af3697b97232d5ae606514f8f7d60f07a842e7d
Lewis Renfrew <lewis.renfrew@linn.co.uk>
2021-03-22 11:22:24 +0000

    reinstate using

--- src/IoC/LoggingModule.cs
+++ src/IoC/LoggingModule.cs
@@ -1,9 +1,9 @@
 ﻿namespace Linn.Stores.IoC
 {
-    using Amazon.SQS;
     using Autofac;
 
     using Linn.Common.Logging;
+    using Linn.Common.Logging.AmazonSqs;
 
     public class LoggingModule : Module
     {
