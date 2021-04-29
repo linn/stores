69096e2db7dc9c7047cd86c30147e103b53db317
Lewis Renfrew <lewis.renfrew@linn.co.uk>
2021-04-29 14:48:39 +0100

    save changes

--- src/Persistence.LinnApps/Repositories/PartDataSheetRepository.cs
+++ src/Persistence.LinnApps/Repositories/PartDataSheetRepository.cs
@@ -28,6 +28,7 @@
         public void Add(PartDataSheet entity)
         {
             this.serviceDbContext.PartDataSheets.Add(entity);
+            this.serviceDbContext.SaveChanges();
         }
 
         public void Remove(PartDataSheet entity)
