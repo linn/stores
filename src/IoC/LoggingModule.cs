42920f75dac5e560c5703e9f5113debe6971ae47
Lewis Renfrew <lewis.renfrew@linn.co.uk>
2021-05-21 16:20:12 +0100

    revert change to LoggingModule

--- src/IoC/LoggingModule.cs
+++ src/IoC/LoggingModule.cs
@@ -10,8 +10,13 @@
     {
         protected override void Load(ContainerBuilder builder)
         {
-
+#if DEBUG
             builder.RegisterType<Linn.Common.Logging.ConsoleLog>().As<ILog>().SingleInstance();
+#else
+                        builder.Register(c => new AmazonSqsLog(c.Resolve<IAmazonSQS>(), LoggingConfiguration.Environment, LoggingConfiguration.MaxInnerExceptionDepth, LoggingConfiguration.AmazonSqsQueueUri))
+                            .As<ILog>()
+                            .SingleInstance();
+#endif
         }
     }
 }
