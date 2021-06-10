6588530ca792f73435c6d784f794eb4578721333
Richard Phillips <richard.phillips@linn.co.uk>
2021-06-10 18:12:16 +0100

    consignment client

--- src/Service.Host/client/src/components/consignments/Consignment.js
+++ src/Service.Host/client/src/components/consignments/Consignment.js
@@ -1,23 +1,159 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Grid from '@material-ui/core/Grid';
-import { Loading } from '@linn-it/linn-form-components-library';
+import { Loading, Dropdown } from '@linn-it/linn-form-components-library';
 import PropTypes from 'prop-types';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableRow from '@material-ui/core/TableRow';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
 import Page from '../../containers/Page';
 
-function Consignment({ consignment, loading, requestErrors }) {
+function Consignment({
+    consignment,
+    loading,
+    requestErrors,
+    openConsignments,
+    getConsignment,
+    optionsLoading,
+    startingTab
+}) {
+    const [currentTab, setcurrentTab] = useState(startingTab);
+
+    const TableItem = withStyles(() => ({
+        body: {
+            borderBottom: 0,
+            whiteSpace: 'pre-line',
+            verticalAlign: 'top'
+        }
+    }))(TableCell);
+
+    const openConsignmentOptions = () => {
+        return openConsignments?.map(c => ({
+            id: c.consignmentId,
+            displayText: c.customerName
+        }));
+    };
+
+    const handleSelectConsignment = (_property, newValue) => {
+        getConsignment(newValue);
+        setcurrentTab(1);
+    };
+
+    const handleTabChange = (_event, newValue) => {
+        setcurrentTab(newValue);
+    };
+
+    const showFreight = freigt => {
+        switch (freigt) {
+            case 'S':
+                return 'Surface';
+            case 'A':
+                return 'Air';
+            case 'W':
+                return 'Sea';
+            default:
+                return 'Other';
+        }
+    };
+
     return (
         <Page requestErrors={requestErrors} showRequestErrors>
             <Grid container spacing={3}>
-                <Grid item xs={12}>
-                    <span>Consignment</span>
+                <Grid item xs={2}>
+                    <Typography variant="h6">Consignment</Typography>
                 </Grid>
-                {loading ? (
-                    <Loading />
-                ) : (
-                    <Grid item xs={12}>
-                        ok {consignment ? consignment.consignmentId : 'none'}
-                    </Grid>
-                )}
+                <Grid item xs={10}>
+                    {consignment && (
+                        <Typography variant="h6">
+                            {consignment.consignmentId} {consignment.customerName}
+                        </Typography>
+                    )}
+                </Grid>
+                <div>
+                    <Tabs
+                        value={currentTab}
+                        onChange={handleTabChange}
+                        style={{ paddingBottom: '20px' }}
+                    >
+                        <Tab label="Select" />
+                        <Tab label="Details" />
+                        <Tab label="Consignment Items" />
+                    </Tabs>
+                    {currentTab === 0 && (
+                        <Grid item xs={12}>
+                            <Dropdown
+                                label="Select consignment"
+                                propertyName="consignmentSelect"
+                                items={openConsignmentOptions()}
+                                onChange={handleSelectConsignment}
+                                optionsLoading={optionsLoading}
+                            />
+                        </Grid>
+                    )}
+                    {loading || !consignment ? (
+                        <Loading />
+                    ) : (
+                        currentTab === 1 && (
+                            <>
+                                <Grid item xs={12}>
+                                    <Table style={{ paddingTop: '30px' }}>
+                                        <TableBody>
+                                            <TableRow key="Account">
+                                                <TableItem>Account</TableItem>
+                                                <TableItem>
+                                                    {consignment.salesAccountId}{' '}
+                                                    {consignment.customerName}
+                                                </TableItem>
+                                            </TableRow>
+                                            <TableRow key="Address">
+                                                <TableItem>Address</TableItem>
+                                                <TableItem>
+                                                    {consignment.address &&
+                                                        consignment.address.displayAddress}
+                                                </TableItem>
+                                            </TableRow>
+                                            <TableRow key="Freight">
+                                                <TableItem>Freight</TableItem>
+                                                <TableItem>
+                                                    {showFreight(consignment.shippingMethod)}
+                                                </TableItem>
+                                            </TableRow>
+                                            <TableRow key="Carrier">
+                                                <TableItem>Carrier</TableItem>
+                                                <TableItem>{consignment.carrier}</TableItem>
+                                            </TableRow>
+                                            <TableRow key="Terms">
+                                                <TableItem>Terms</TableItem>
+                                                <TableItem>{consignment.terms}</TableItem>
+                                            </TableRow>
+                                            <TableRow key="Hub">
+                                                <TableItem>Hub</TableItem>
+                                                <TableItem>{consignment.hubId}</TableItem>
+                                            </TableRow>
+                                            <TableRow key="DateOpened">
+                                                <TableItem>Date Opened</TableItem>
+                                                <TableItem>{consignment.dateOpened}</TableItem>
+                                            </TableRow>
+                                            <TableRow key="DateClosed">
+                                                <TableItem>Date Closed</TableItem>
+                                                <TableItem>{consignment.dateClosed}</TableItem>
+                                                <TableItem>Closed By</TableItem>
+                                                <TableItem>
+                                                    {consignment.closedBy &&
+                                                        consignment.closedBy.fullName}
+                                                </TableItem>
+                                            </TableRow>
+                                        </TableBody>
+                                    </Table>
+                                </Grid>
+                            </>
+                        )
+                    )}
+                </div>
             </Grid>
         </Page>
     );
@@ -25,18 +161,35 @@ function Consignment({ consignment, loading, requestErrors }) {
 
 Consignment.propTypes = {
     consignment: PropTypes.shape({
-        consignmentId: PropTypes.number
+        consignmentId: PropTypes.number,
+        customerName: PropTypes.string,
+        salesAccountId: PropTypes.number,
+        shippingMethod: PropTypes.string,
+        dateOpened: PropTypes.string,
+        dateClosed: PropTypes.string,
+        carrier: PropTypes.string,
+        terms: PropTypes.string,
+        hubId: PropTypes.number,
+        closedBy: PropTypes.shape({ id: PropTypes.number, fullName: PropTypes.string }),
+        address: PropTypes.shape({ id: PropTypes.number, displayAddress: PropTypes.string })
     }),
     loading: PropTypes.bool,
     requestErrors: PropTypes.arrayOf(
         PropTypes.shape({ message: PropTypes.string, name: PropTypes.string })
-    )
+    ),
+    openConsignments: PropTypes.arrayOf(PropTypes.shape({})),
+    getConsignment: PropTypes.func.isRequired,
+    optionsLoading: PropTypes.bool,
+    startingTab: PropTypes.number
 };
 
 Consignment.defaultProps = {
     consignment: null,
     loading: false,
-    requestErrors: null
+    requestErrors: null,
+    openConsignments: [],
+    optionsLoading: false,
+    startingTab: 0
 };
 
 export default Consignment;
--- src/Service.Host/client/src/containers/consignments/Consignment.js
+++ src/Service.Host/client/src/containers/consignments/Consignment.js
@@ -4,26 +4,48 @@ import {
     initialiseOnMount,
     getRequestErrors
 } from '@linn-it/linn-form-components-library';
+import queryString from 'query-string';
 import { getUserNumber } from '../../selectors/userSelectors';
 import Consignment from '../../components/consignments/Consignment';
 import consignmentSelectors from '../../selectors/consignmentSelectors';
 import consignmentActions from '../../actions/consignmentActions';
+import consignmentsSelectors from '../../selectors/consignmentsSelectors';
+import consignmentsActions from '../../actions/consignmentsActions';
 import * as itemTypes from '../../itemTypes';
 
-const mapStateToProps = state => ({
+const getOptions = ownProps => {
+    const options = queryString.parse(ownProps.location.search);
+    return options;
+};
+
+const initialise = ({ options }) => dispatch => {
+    if (options.consignmentId) {
+        dispatch(consignmentActions.fetch(options.consignmentId));
+    }
+
+    dispatch(consignmentsActions.fetch());
+};
+
+const mapStateToProps = (state, ownProps) => ({
     consignment: consignmentSelectors.getItem(state),
     loading: consignmentSelectors.getLoading(state),
     snackbarVisible: consignmentSelectors.getSnackbarVisible(state),
     itemError: getItemError(state, itemTypes.consignment.item),
     userNumber: getUserNumber(state),
-    requestErrors: getRequestErrors(state)?.filter(error => error.type !== 'FETCH_ERROR')
+    requestErrors: getRequestErrors(state)?.filter(error => error.type !== 'FETCH_ERROR'),
+    openConsignments: consignmentsSelectors.getItems(state),
+    optionsLoading: consignmentsSelectors.getLoading(state),
+    options: getOptions(ownProps),
+    startingTab: getOptions(ownProps).consignmentId ? 1 : 0
 });
 
 const mapDispatchToProps = {
+    initialise,
     addItem: consignmentActions.add,
     updateItem: consignmentActions.update,
     setEditStatus: consignmentActions.setEditStatus,
-    setSnackbarVisible: consignmentActions.setSnackbarVisible
+    setSnackbarVisible: consignmentActions.setSnackbarVisible,
+    getConsignment: consignmentActions.fetch
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(initialiseOnMount(Consignment));
