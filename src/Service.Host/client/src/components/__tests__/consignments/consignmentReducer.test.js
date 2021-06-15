d96a20a6958357fe4ce4703d25c641e4ddb8b5a7
Richard Phillips <richard.phillips@linn.co.uk>
2021-06-15 12:18:10 +0100

    reducer test

---
+++ src/Service.Host/client/src/components/__tests__/consignments/consignmentReducer.test.js
@@ -0,0 +1,53 @@
+import consignmentReducer from '../../consignments/consignmentReducer';
+
+describe('consignmentReducer tests', () => {
+    it('should initialise on load', () => {
+        expect(
+            consignmentReducer(
+                {},
+                {
+                    type: 'initialise',
+                    payload: {
+                        consignmentNumber: 123,
+                        status: 'L'
+                    }
+                }
+            )
+        ).toEqual({
+            consignment: {
+                consignmentNumber: 123,
+                status: 'L'
+            },
+            originalConsignment: {
+                consignmentNumber: 123,
+                status: 'L'
+            }
+        });
+    });
+
+    it('should reset', () => {
+        const state = {
+            consignment: { consignmentId: 123, hubId: 4 },
+            originalConsignment: { consignmentId: 123, hubId: 5 }
+        };
+        const expectedState = {
+            consignment: { consignmentId: 123, hubId: 5 },
+            originalConsignment: { consignmentId: 123, hubId: 5 }
+        };
+
+        expect(
+            consignmentReducer(state, {
+                type: 'reset',
+                payload: null
+            })
+        ).toEqual(expectedState);
+    });
+
+    it('should update field', () => {
+        const state = { consignment: { consignmentId: 123, hubId: 4 } };
+        const expectedState = { consignment: { consignmentId: 123, hubId: 5 } };
+        expect(
+            consignmentReducer(state, { type: 'updateField', fieldName: 'hubId', payload: 5 })
+        ).toEqual(expectedState);
+    });
+});
