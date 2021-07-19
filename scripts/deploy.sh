03fd397c237f7520577215653d9ef3fda39e53e4
Richard Phillips <richard.phillips@linn.co.uk>
2021-07-19 16:28:27 +0100

    default install loc

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,8 +4,7 @@ set -ev
 # install aws cli
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
-sudo ./aws/install -b ~/bin/aws
-export PATH=~/bin:$PATH
+sudo ./aws/install
 
 # deploy on aws
 if [ "${TRAVIS_BRANCH}" = "master" ]; then
