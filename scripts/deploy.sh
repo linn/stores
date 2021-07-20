350e89205d78482afaf6fcf5a9e4cb2fcecf64bd
Richard Phillips <richardiphillips@users.noreply.github.com>
2021-07-20 08:36:18 +0100

    first attempt at new aws client (#192)
    * first attempt at new aws client
    
    * sudo allowed?
    
    * default install loc

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,10 +2,9 @@
 set -ev
 
 # install aws cli
-curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
-unzip awscli-bundle.zip
-./awscli-bundle/install -b ~/bin/aws
-export PATH=~/bin:$PATH
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
 
 # deploy on aws
 if [ "${TRAVIS_BRANCH}" = "master" ]; then
