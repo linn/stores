86701fd951d8f23cf4be050e9f2615fddd4f0325
= <lewis.renfrew@linn.co.uk>
2021-09-03 12:45:14 +0100

    ammend test

--- tests/Unit/Domain.LinnApps.Tests/GoodsInServiceTests/WhenBookingInAndStoredProcedureCallFails.cs
+++ tests/Unit/Domain.LinnApps.Tests/GoodsInServiceTests/WhenBookingInAndStoredProcedureCallFails.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
     using System.Linq.Expressions;
 
     using FluentAssertions;
@@ -136,6 +137,7 @@
         {
             this.result.Success.Should().BeFalse();
             this.result.Message.Should().Be("Something went wrong...");
+            this.result.Lines.Should().BeNullOrEmpty();
         }
     }
 }
