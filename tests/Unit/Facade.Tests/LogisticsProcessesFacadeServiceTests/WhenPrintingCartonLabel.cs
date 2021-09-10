cda212b845d9b466e96ac64f2e5eedc606800335
Richard Phillips <richardiphillips@users.noreply.github.com>
2021-09-10 15:39:44 +0100

    show and reprint consignment documents  (#222)
    * get invs and export books
    
    * display invoices tab
    
    * print consignment documents service
    
    * wire in print consignment documents
    
    * add print docs button functions

--- tests/Unit/Facade.Tests/LogisticsProcessesFacadeServiceTests/WhenPrintingCartonLabel.cs
+++ tests/Unit/Facade.Tests/LogisticsProcessesFacadeServiceTests/WhenPrintingCartonLabel.cs
@@ -1,20 +1,20 @@
-﻿namespace Linn.Stores.Facade.Tests.LogisticsLabelFacadeServiceTests
+﻿namespace Linn.Stores.Facade.Tests.LogisticsProcessesFacadeServiceTests
 {
     using FluentAssertions;
 
     using Linn.Common.Facade;
-    using Linn.Stores.Domain.LinnApps.Exceptions;
     using Linn.Stores.Domain.LinnApps.Models;
     using Linn.Stores.Resources.RequestResources;
 
     using NSubstitute;
-    using NSubstitute.ExceptionExtensions;
 
     using NUnit.Framework;
 
-    public class WhenPrintingCartonLabelFails : ContextBase
+    public class WhenPrintingCartonLabel : ContextBase
     {
         private LogisticsLabelRequestResource resource;
+
+        private ProcessResult serviceResult;
 
         private IResult<ProcessResult> result;
 
@@ -30,13 +30,14 @@
                                     UserNumber = 123,
                                     NumberOfCopies = 2
                                 };
+            this.serviceResult = new ProcessResult(true, "ok");
             this.LogisticsLabelService.PrintCartonLabel(
                     this.resource.ConsignmentId,
                     this.resource.FirstItem,
                     this.resource.LastItem, 
                     this.resource.UserNumber,
                     this.resource.NumberOfCopies)
-                .Throws(new ProcessException("Failed to print carton label"));
+                .Returns(this.serviceResult);
 
             this.result = this.Sut.PrintLabel(this.resource);
         }
@@ -55,9 +56,10 @@
         [Test]
         public void ShouldReturnSuccess()
         {
-            this.result.Should().BeOfType<BadRequestResult<ProcessResult>>();
-            var dataResult = (BadRequestResult<ProcessResult>)this.result;
-            dataResult.Message.Should().Be("Failed to print carton label");
+            this.result.Should().BeOfType<SuccessResult<ProcessResult>>();
+            var dataResult = ((SuccessResult<ProcessResult>)this.result).Data;
+            dataResult.Message.Should().Be("ok");
+            dataResult.Success.Should().BeTrue();
         }
     }
 }
---
+++ tests/Unit/Facade.Tests/LogisticsProcessesFacadeServiceTests/WhenPrintingConsignmentDocuments.cs
@@ -0,0 +1,53 @@
+﻿namespace Linn.Stores.Facade.Tests.LogisticsProcessesFacadeServiceTests
+{
+    using FluentAssertions;
+
+    using Linn.Common.Facade;
+    using Linn.Stores.Domain.LinnApps.Models;
+    using Linn.Stores.Resources.RequestResources;
+
+    using NSubstitute;
+
+    using NUnit.Framework;
+
+    public class WhenPrintingConsignmentDocuments : ContextBase
+    {
+        private PrintConsignmentDocumentsRequestResource resource;
+
+        private ProcessResult serviceResult;
+
+        private IResult<ProcessResult> result;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.resource = new PrintConsignmentDocumentsRequestResource
+                                {
+                                    ConsignmentId = 880,
+                                    UserNumber = 123
+                                };
+            this.serviceResult = new ProcessResult(true, "ok");
+            this.ConsignmentService.PrintConsignmentDocuments(this.resource.ConsignmentId, this.resource.UserNumber)
+                .Returns(this.serviceResult);
+
+            this.result = this.Sut.PrintConsignmentDocuments(this.resource);
+        }
+
+        [Test]
+        public void ShouldCallService()
+        {
+            this.ConsignmentService.Received().PrintConsignmentDocuments(
+                this.resource.ConsignmentId,
+                this.resource.UserNumber);
+        }
+
+        [Test]
+        public void ShouldReturnSuccess()
+        {
+            this.result.Should().BeOfType<SuccessResult<ProcessResult>>();
+            var dataResult = ((SuccessResult<ProcessResult>)this.result).Data;
+            dataResult.Message.Should().Be("ok");
+            dataResult.Success.Should().BeTrue();
+        }
+    }
+}
