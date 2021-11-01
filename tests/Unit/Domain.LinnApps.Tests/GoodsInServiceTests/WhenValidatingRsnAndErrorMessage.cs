45a4b9cae7862187794957dcd5bd289c03a8111d
= <lewis.renfrew@linn.co.uk>
2021-11-01 17:03:11 +0000

    add missing unit tests

---
+++ tests/Unit/Domain.LinnApps.Tests/GoodsInServiceTests/WhenValidatingRsnAndErrorMessage.cs
@@ -0,0 +1,40 @@
+﻿namespace Linn.Stores.Domain.LinnApps.Tests.GoodsInServiceTests
+{
+    using FluentAssertions;
+
+    using Linn.Stores.Domain.LinnApps.GoodsIn;
+
+    using NSubstitute;
+
+    using NUnit.Framework;
+
+    public class WhenValidatingRsnAndErrorMessage : ContextBase
+    {
+        private ValidateRsnResult result;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.GoodsInPack.When(x => x.GetRsnDetails(
+                1,
+                out _,
+                out _,
+                out _,
+                out _,
+                out _,
+                out _)).Do(x =>
+                {
+                    x[6] = "Can't book in";
+                });
+
+            this.result = this.Sut.ValidateRsn(1);
+        }
+
+        [Test]
+        public void ShouldReturnErrorResult()
+        {
+            this.result.Success.Should().BeFalse();
+            this.result.Message.Should().Be("Can't book in");
+        }
+    }
+}
