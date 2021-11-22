d3744243172647370165124e2ce5d3cc9fb32390
= <lewis.renfrew@linn.co.uk>
2021-11-22 10:12:06 +0000

    reload data when receive actions via middleware

--- src/Service.Host/client/src/middleware/receiveUpdatedStockLocator.js
+++ src/Service.Host/client/src/middleware/receiveUpdatedStockLocator.js
@@ -5,6 +5,8 @@ export default ({ dispatch }) => next => action => {
     const result = next(action);
     switch (action.type) {
         case actionTypes.stockLocatorActionTypes.RECEIVE_UPDATED_STOCK_LOCATOR:
+        case actionTypes.stockLocatorActionTypes.RECEIVE_NEW_STOCK_LOCATOR:
+        case actionTypes.stockLocatorActionTypes.RECEIVE_DELETED_LOCATOR:
             if (action.payload.partId) {
                 dispatch(
                     stockLocatorsActions.searchWithOptions(
