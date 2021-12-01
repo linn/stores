c970b91a889c630be0af8838f965963c187abef3
rosss <rosss@PC1329>
2021-12-01 17:31:36 +0000

    add dropdown for parttemplate search screen. Remove unnecessary code.

--- src/Service.Host/client/src/components/parts/PartTemplatesSearch.js
+++ src/Service.Host/client/src/components/parts/PartTemplatesSearch.js
@@ -1,213 +1,89 @@
-import React, { useEffect, useState, useReducer } from 'react';
+import React, { useState } from 'react';
 import Grid from '@material-ui/core/Grid';
-import {
-    SearchInputField,
-    LinkButton,
-    useSearch,
-    PaginatedTable,
-    Loading
-} from '@linn-it/linn-form-components-library';
+import { LinkButton, Dropdown } from '@linn-it/linn-form-components-library';
 import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/styles';
-import queryString from 'query-string';
+import makeStyles from '@material-ui/styles/makeStyles';
+import Typography from '@material-ui/core/Typography';
 import Page from '../../containers/Page';
 
-function PartTemplatesSearch({ items, fetchItems, loading, history, privileges }) {
-    const [pageOptions, setPageOptions] = useState({
-        orderBy: '',
-        orderAscending: false,
-        currentPage: 0,
-        rowsPerPage: 10
-    });
+const useStyles = makeStyles(theme => ({
+    button: {
+        marginLeft: theme.spacing(1),
+        marginTop: theme.spacing(3)
+    },
+    a: {
+        textDecoration: 'none'
+    }
+}));
 
-    const [rowsToDisplay, setRowsToDisplay] = useState([]);
-
-    const allowedToCreate = () => {
-        return privileges?.some(priv => priv === 'part.admin');
-    };
-
-    const useStyles = makeStyles(theme => ({
-        marginTop1: {
-            marginTop: theme.spacing(1),
-            display: 'inline-block',
-            width: '2em'
-        },
-        displayInline: {
-            display: 'inline'
-        }
-    }));
+function PartTemplateSearch({ privileges, partTemplates, linkToSources }) {
     const classes = useStyles();
 
-    function partTemplateReducer(state, action) {
-        switch (action.type) {
-            case 'updateSearchTerms': {
-                const newSearchTerms = {
-                    ...state.searchTerms,
-                    [action.searchTermName]: action.newValue
-                };
-                return {
-                    ...state,
-                    searchTerms: newSearchTerms,
-                    stringifiedSearch: `&${queryString.stringify(newSearchTerms, '&', '=')}`
-                };
-            }
-            default:
-                return state;
-        }
-    }
+    const [template, setTemplate] = useState();
 
-    const [state, dispatch] = useReducer(partTemplateReducer, {
-        stringifiedSearch: '',
-        searchTerms: {
-            partTemplateNumberSearchTerm: ''
+    const createUrl = () => {
+        if (linkToSources) {
+            return '/inventory/part-templates/create';
         }
-    });
-
-    const handleSearchTermChange = (propertyName, newValue) => {
-        dispatch({ type: 'updateSearchTerms', searchTermName: propertyName, newValue });
+        return template ? `/inventory/part-templates/create` : '/inventory/part-templates/create';
     };
-
-    useSearch(fetchItems, state.stringifiedSearch, null, 'searchTerm');
-
-    const handleRowLinkClick = href => history.push(href);
-
-    useEffect(() => {
-        const compare = (field, orderAscending) => (a, b) => {
-            if (!field) {
-                return 0;
-            }
-
-            if (a[field] < b[field]) {
-                return orderAscending ? -1 : 1;
-            }
-
-            if (a[field] > b[field]) {
-                return orderAscending ? 1 : -1;
-            }
-
-            return 0;
-        };
-        const rows = items
-            ? items.map(el => ({
-                  partRoot: el.partRoot,
-                  description: el.description,
-                  hasDataSheet: el.hasDataSheet,
-                  hasNumberSequence: el.hasNumberSequence,
-                  nextNumber: el.nextNumber,
-                  allowVariants: el.allowVariants,
-                  variants: el.variants,
-                  accountingCompany: el.accountingCompany,
-                  productCode: el.productCode,
-                  stockControlled: el.stockControlled,
-                  linnProduced: el.linnProduced,
-                  rmfgCode: el.rmfgCode,
-                  bomType: el.bomType,
-                  assemblyTechnology: el.assemblyTechnology,
-                  allowPartCreation: el.allowPartCreation,
-                  paretoCode: el.paretoCode
-              }))
-            : [];
-
-        if (!rows || rows.length === 0) {
-            setRowsToDisplay([]);
-        } else {
-            setRowsToDisplay(
-                rows
-                    .sort(compare(pageOptions.orderBy, pageOptions.orderAscending))
-                    .slice(
-                        pageOptions.currentPage * pageOptions.rowsPerPage,
-                        pageOptions.currentPage * pageOptions.rowsPerPage + pageOptions.rowsPerPage
-                    )
-            );
+    const canCreate = () => {
+        if (!(privileges.length < 1)) {
+            return privileges.some(priv => priv === 'part.admin');
         }
-    }, [
-        pageOptions.currentPage,
-        pageOptions.rowsPerPage,
-        pageOptions.orderBy,
-        pageOptions.orderAscending,
-        items
-    ]);
-
-    const columns = {
-        partRoot: 'Part Root',
-        description: 'Description',
-        hasDataSheet: 'Has Data Sheet',
-        hasNumberSequence: 'Has Number Sequence',
-        nextNumber: 'Next Number',
-        allowVariants: 'Allow Variants',
-        variants: 'Variants',
-        accountingCompany: 'Accounting Company',
-        productCode: 'Product Code',
-        stockControlled: 'Stock Controlled',
-        linnProduced: 'Linn Produced',
-        rmfgCode: 'RM FG Code',
-        bomType: 'BOM Type',
-        assemblyTechnology: 'Assembly Technology',
-        allowPartCreation: 'Allow Part Creation',
-        paretoCode: 'Pareto Code'
+        return false;
     };
 
     return (
         <Page>
-            <Grid spacing={3} container justifyContent="center">
-                <Grid item xs={11} />
+            <Grid container spacing={3}>
+                <Grid item xs={7}>
+                    <Typography variant="h3">Part Template Utility</Typography>
+                </Grid>
+                <Grid item xs={3}>
+                    {!linkToSources && (
+                        <Dropdown
+                            label="Template"
+                            propertyName="partTemplate"
+                            items={partTemplates
+                                .filter(p => p.allowPartCreation === 'Y')
+                                .map(t => ({
+                                    id: t.partRoot,
+                                    displayText: `${t.partRoot} - ${t.description}`
+                                }))}
+                            fullWidth
+                            allowNoValue
+                            value={template}
+                            onChange={(_, newValue) => {
+                                setTemplate(newValue);
+                            }}
+                        />
+                    )}
+                </Grid>
                 <Grid item xs={1}>
                     <LinkButton
                         text="Create"
-                        to="/inventory/part-templates/create"
-                        disabled={!allowedToCreate()}
+                        to={createUrl()}
+                        disabled={!canCreate()}
+                        tooltip={canCreate() ? null : 'You are not authorised to create parts.'}
                     />
                 </Grid>
-                <Grid item xs={3}>
-                    <SearchInputField
-                        label="Part Template Part Root Search"
-                        fullWidth
-                        placeholder="search.."
-                        onChange={handleSearchTermChange}
-                        propertyName="partRoot"
-                        type="text"
-                        value={state.searchTerms.partTemplateNumberSearchTerm}
-                    />
-                </Grid>
-
-                <Grid item xs={12}>
-                    {loading ? (
-                        <Loading />
-                    ) : (
-                        <>
-                            {rowsToDisplay.length > 0 && (
-                                <PaginatedTable
-                                    columns={columns}
-                                    handleRowLinkClick={handleRowLinkClick}
-                                    rows={rowsToDisplay}
-                                    pageOptions={pageOptions}
-                                    setPageOptions={setPageOptions}
-                                    totalItemCount={items ? items.length : 0}
-                                    expandable={false}
-                                />
-                            )}
-                        </>
-                    )}
-                </Grid>
+                <Grid item xs={1} />
             </Grid>
         </Page>
     );
 }
 
-PartTemplatesSearch.propTypes = {
-    items: PropTypes.arrayOf(
-        PropTypes.shape({
-            partTemplateNumber: PropTypes.number
-        })
-    ).isRequired,
-    loading: PropTypes.bool,
-    fetchItems: PropTypes.func.isRequired,
-    history: PropTypes.shape({ push: PropTypes.func }).isRequired,
-    privileges: PropTypes.arrayOf(PropTypes.string).isRequired
+PartTemplateSearch.propTypes = {
+    history: PropTypes.shape({}).isRequired,
+    privileges: PropTypes.arrayOf(PropTypes.string).isRequired,
+    partTemplates: PropTypes.arrayOf(PropTypes.shape({})),
+    linkToSources: PropTypes.bool
 };
 
-PartTemplatesSearch.defaultProps = {
-    loading: false
+PartTemplateSearch.defaultProps = {
+    partTemplates: [],
+    linkToSources: false
 };
 
-export default PartTemplatesSearch;
+export default PartTemplateSearch;
--- src/Service.Host/client/src/containers/parts/PartTemplates.js
+++ src/Service.Host/client/src/containers/parts/PartTemplates.js
@@ -2,16 +2,25 @@ import { connect } from 'react-redux';
 import { initialiseOnMount } from '@linn-it/linn-form-components-library';
 import partTemplatesActions from '../../actions/partTemplatesActions';
 import partTemplatesSelectors from '../../selectors/partTemplatesSelectors';
+import partTemplateSelectors from '../../selectors/partTemplateSelectors';
 import PartTemplatesSearch from '../../components/parts/PartTemplatesSearch';
 import { getPrivileges } from '../../selectors/userSelectors';
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state, { match }) => ({
     items: partTemplatesSelectors.getItems(state),
     loading: partTemplatesSelectors.getLoading(state),
-    privileges: getPrivileges(state)
+    linkToSources: match?.url?.endsWith('/sources'),
+    privileges: getPrivileges(state),
+    partTemplates: partTemplatesSelectors.getItems(state),
+    partTemplate: partTemplateSelectors.getItem(state)
 });
 
+const initialise = () => dispatch => {
+    dispatch(partTemplatesActions.fetch());
+};
+
 const mapDispatchToProps = {
+    initialise,
     fetchItems: partTemplatesActions.fetchByQueryString,
     classes: {}
 };
