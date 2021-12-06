e91b4480cf63026b1d7c642156251e12f377d3d0
= <lewis.renfrew@linn.co.uk>
2021-12-06 12:50:54 +0000

    test to prevent future typos

--- tests/Unit/Domain.LinnApps.Tests/PartServiceTests/WhenCreatingFromTemplate.cs
+++ tests/Unit/Domain.LinnApps.Tests/PartServiceTests/WhenCreatingFromTemplate.cs
@@ -28,7 +28,7 @@
             this.privileges = new List<string> { "part.admin" };
 
             this.AuthService.HasPermissionFor(AuthorisedAction.PartAdmin, this.privileges).Returns(true);
-            this.PartRepository.SearchPartsWithWildcard(Arg.Any<string>(), Arg.Any<string>(), Arg.Any<bool>(), Arg.Any<int>())
+            this.PartRepository.SearchPartsWithWildcard("CAP %", Arg.Any<string>(), Arg.Any<bool>(), Arg.Any<int>())
                 .Returns(new List<Part>
                              {
                                  new Part
@@ -39,9 +39,19 @@
             this.PartRepository.FilterBy(Arg.Any<Expression<Func<Part, bool>>>())
                 .Returns(new List<Part>().AsQueryable());
 
-            this.TemplateRepository.FindById(Arg.Any<string>()).Returns(new PartTemplate { HasNumberSequence = "Y", NextNumber = 1 });
+            this.TemplateRepository.FindById(Arg.Any<string>()).Returns(new PartTemplate { HasNumberSequence = "Y", NextNumber = 1, PartRoot = "CAP" });
             this.PartPack.PartRoot(Arg.Any<string>()).Returns("CAP");
             this.Sut.CreatePart(this.part, this.privileges, true);
+        }
+
+        [Test]
+        public void ShouldSearchForNewestPartOfThisType()
+        {
+            this.PartRepository.Received().SearchPartsWithWildcard(
+                "CAP %",
+                Arg.Any<string>(),
+                Arg.Any<bool>(),
+                Arg.Any<int>());
         }
 
         [Test]
