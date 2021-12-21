0628bb0b24c3a4cd490b389c16bcd094c42137b8
iaincrawf <iain.crawford@linn.co.uk>
2021-12-21 16:22:12 +0000

    added describes and beforeEach render() methods

--- src/Service.Host/client/src/components/__tests__/parts/PartTemplate.test.js
+++ src/Service.Host/client/src/components/__tests__/parts/PartTemplate.test.js
@@ -36,54 +36,62 @@ const item = {
     links: { href: '', rel: 'self' }
 };
 
-test('On Create - page renders all fields without crashing...', () => {
-    const { getByText, queryByText } = render(
-        <PartTemplate
-            editStatus="create"
-            searchProductAnalysisCodes={searchProductAnalysisCodes}
-            clearProductAnalysisCodesSearch={clearProductAnalysisCodesSearch}
-            setSnackbarVisible={setSnackbarVisible}
-            history={history}
-            addItem={addItem}
-            updateItem={updateItem}
-            setEditStatus={setEditStatus}
-            privileges={['part.admin']}
-            itemId={1}
-        />
-    );
-    expect(queryByText('PartRoot')).toBeNull();
-    expect(getByText('linnProduced')).toBeNull();
+describe('On Create', () => {
+    beforeEach(() => {
+        render(
+            <PartTemplate
+                editStatus="create"
+                searchProductAnalysisCodes={searchProductAnalysisCodes}
+                clearProductAnalysisCodesSearch={clearProductAnalysisCodesSearch}
+                setSnackbarVisible={setSnackbarVisible}
+                history={history}
+                addItem={addItem}
+                updateItem={updateItem}
+                setEditStatus={setEditStatus}
+                privileges={['part.admin']}
+                itemId={1}
+            />
+        );
+    });
+
+    test('page renders all fields without crashing...', () => {
+        expect(screen.queryByText('PartRoot')).toBeNull();
+    });
+
+    test('save button should be disabled', () => {
+        expect(screen.getAllByRole('button', { name: 'save-button' })).toHaveClass('Mui-disabled');
+    });
+
+    test('Should Render Title', () => {
+        expect(screen.getByText('Create New Part Template')).toBeInTheDocument();
+    });
 });
 
-test('save button should be disabled', () => {
-    expect(screen.getAllByRole('button', { name: 'save-button' })).toHaveClass('Mui-disabled')
+describe('On View', () => {
+    beforeEach(() => {
+        render(
+            <PartTemplate
+                editStatus="view"
+                searchProductAnalysisCodes={searchProductAnalysisCodes}
+                clearProductAnalysisCodesSearch={clearProductAnalysisCodesSearch}
+                setSnackbarVisible={setSnackbarVisible}
+                history={history}
+                addItem={addItem}
+                updateItem={updateItem}
+                setEditStatus={setEditStatus}
+                item={item}
+                privileges={['part.admin']}
+            />
+        );
+    });
 
-});
+    test('Page renders populated fields', () => {
+        expect(screen.getAllByText('Part Root')[0]).toBeInTheDocument();
+        expect(screen.getByDisplayValue('REACT')).toBeInTheDocument();
 
-test('Should Render Title', () => {
-    expect(screen.getByDisplayValue('Part Template Details')).toBeInTheDocument();
-});
-
-test('On View -  page renders populated fields', () => {
-    const { getByText, getByDisplayValue } = render(
-        <PartTemplate
-            editStatus="view"
-            searchProductAnalysisCodes={searchProductAnalysisCodes}
-            clearProductAnalysisCodesSearch={clearProductAnalysisCodesSearch}
-            setSnackbarVisible={setSnackbarVisible}
-            history={history}
-            addItem={addItem}
-            updateItem={updateItem}
-            setEditStatus={setEditStatus}
-            item={item}
-            privileges={['part.admin']}
-        />
-    );
-    expect(getByText('Part Root')).toBeInTheDocument();
-    expect(getByDisplayValue('REACT')).toBeInTheDocument();
-
-    expect(getByText('Seq Next Number')).toBeInTheDocument();
-    expect(getByDisplayValue('123')).toBeInTheDocument();
+        expect(screen.getByText('Seq Next Number')).toBeInTheDocument();
+        expect(screen.getByDisplayValue('123')).toBeInTheDocument();
+    });
 });
 
 describe('When have no permissions/privileges', () => {
@@ -105,6 +113,6 @@ describe('When have no permissions/privileges', () => {
     });
     test('cannot edit fields', () => {
         expect(screen.getByText('Part Root')).not.toBeDisabled();
-      //  screen.debug(undefined, Infinity);
+        //  screen.debug(undefined, Infinity);
     });
 });
