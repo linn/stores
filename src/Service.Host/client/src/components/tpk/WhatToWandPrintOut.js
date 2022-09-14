33c582184682ab1354e33ab14093f650354ea7b9
Lewis Renfrew <lewis.renfrew@linn.co.uk>
2022-09-14 11:29:11 +0100

    fix layout

--- src/Service.Host/client/src/components/tpk/WhatToWandPrintOut.js
+++ src/Service.Host/client/src/components/tpk/WhatToWandPrintOut.js
@@ -10,112 +10,85 @@ export default class WhatToWandPrintout extends React.PureComponent {
             <div className="show-only-when-printing">
                 <style>
                     {`@media print {
-                @page { size: portrait; margin: 100px; }
+                @page { size: portrait; margin: 75px; }
               }`}
                 </style>
-                <Grid container spacing={3}>
-                    {whatToWandReport.map(c => {
-                        const rows = c.lines.map(l => ({
-                            ...l,
-                            id: `${l.orderNumber - l.orderLine}`
-                        }));
-                        return (
-                            <>
-                                <Grid item xs={1} />
-                                <Grid item xs={3}>
-                                    What to Wand
-                                </Grid>
-                                <Grid item xs={5} />
-                                <Grid item xs={3}>
-                                    {new Date().toLocaleString()}
-                                </Grid>
-                                <Grid item xs={12}>
-                                    <Typography align="center" variant="h2">
-                                        {c.type}
-                                    </Typography>
-                                </Grid>
-                                <Grid item xs={12}>
-                                    <Typography align="center" variant="h4">
-                                        {c.account.accountName}
-                                    </Typography>
-                                </Grid>
-                                <Grid item xs={1} />
-                                <Grid item xs={11}>
-                                    <Typography variant="h2">
-                                        {c.consignment.consignmentId}
-                                    </Typography>
-                                </Grid>
-                                <Grid item xs={1} />
-                                <Grid item xs={2}>
-                                    Account:
-                                </Grid>
-                                <Grid item xs={2}>
-                                    {c.account.accountId}
-                                </Grid>
-                                <Grid item xs={2}>
+
+                {whatToWandReport.map(c => {
+                    const rows = c.lines.map(l => ({
+                        ...l,
+                        id: `${l.orderNumber - l.orderLine}`
+                    }));
+                    return (
+                        <Grid spacing={3} style={{ breakAfter: 'always !important' }}>
+                            <Grid item xs={12}>
+                                <Typography align="center" variant="h2">
+                                    {c.type}
+                                </Typography>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <Typography align="center" variant="h4">
                                     {c.account.accountName}
-                                </Grid>
-                                <Grid item xs={5} />
-                                <Grid item xs={1} />
-                                <Grid item xs={2}>
-                                    Delivery Address:
-                                </Grid>
-                                <Grid item xs={2}>
-                                    {c.consignment.addressId}
-                                </Grid>
-                                <Grid item xs={8} />
-                                <Grid item xs={2}>
-                                    Country:
-                                </Grid>
-                                <Grid item xs={4}>
-                                    {c.consignment.country}
-                                </Grid>
-                                <Grid item xs={5} />
-                                <Grid item xs={1} />
-                                <Grid item xs={12}>
-                                    <table style={{ width: '100%', borderSpacing: '0 20px' }}>
-                                        <thead style={{ textAlign: 'left', marginTop: '20px' }}>
-                                            <tr>
-                                                <th style={{ width: '15%' }}>Order</th>
-                                                <th style={{ width: '5%' }}>Line</th>
-                                                <th style={{ width: '15%' }}>Article</th>
-                                                <th style={{ width: '20%' }}>Invoice Desc.</th>
-                                                <th style={{ width: '15%' }}>Mains Lead</th>
-                                                <th style={{ width: '5%' }}>Kitted</th>
-                                                <th style={{ width: '5%' }}>Ordered</th>
-                                                <th style={{ width: '5%' }}>SIF</th>
+                                </Typography>
+                            </Grid>
+                            <Grid item xs={1} />
+                            <Grid item xs={11}>
+                                <Typography variant="h2">{c.consignment.consignmentId}</Typography>
+                            </Grid>
+                            <Grid item xs={1} />
+                            <Grid item xs={12}>
+                                Account: {c.account.accountId} - {c.account.accountName}
+                            </Grid>
+
+                            <Grid item xs={12}>
+                                Delivery Address: {c.consignment.addressId}
+                            </Grid>
+                            <Grid item xs={12}>
+                                Country: {c.consignment.country}
+                            </Grid>
+                            <Grid item xs={5} />
+                            <Grid item xs={1} />
+                            <Grid item xs={12}>
+                                <table style={{ width: '100%', borderSpacing: '0 20px' }}>
+                                    <tr style={{ textAlign: 'left', marginTop: '20px' }}>
+                                        <th style={{ width: '15%' }}>Order</th>
+                                        <th style={{ width: '5%' }}>Line</th>
+                                        <th style={{ width: '15%' }}>Article</th>
+                                        <th style={{ width: '20%' }}>Invoice Desc.</th>
+                                        <th style={{ width: '15%' }}>Mains Lead</th>
+                                        <th style={{ width: '5%' }}>Kitted</th>
+                                        <th style={{ width: '5%' }}>Ordered</th>
+                                        <th style={{ width: '5%' }}>SIF</th>
+                                    </tr>
+                                    <tbody>
+                                        {rows.map(r => (
+                                            <tr style={{ paddingTop: '10px' }}>
+                                                <td>{r.orderNumber}</td>
+                                                <td>{r.orderLine}</td>
+                                                <td>{r.articleNumber}</td>
+                                                <td>{r.invoiceDescription}</td>
+                                                <td>{r.mainsLead}</td>
+                                                <td>{r.kitted}</td>
+                                                <td>{r.ordered}</td>
+                                                <td>{r.sif}</td>
                                             </tr>
-                                        </thead>
-                                        <tbody>
-                                            {rows.map(r => (
-                                                <tr style={{ paddingTop: '10px' }}>
-                                                    <td>{r.orderNumber}</td>
-                                                    <td>{r.orderLine}</td>
-                                                    <td>{r.articleNumber}</td>
-                                                    <td>{r.invoiceDescription}</td>
-                                                    <td>{r.mainsLead}</td>
-                                                    <td>{r.kitted}</td>
-                                                    <td>{r.ordered}</td>
-                                                    <td>{r.sif}</td>
-                                                </tr>
-                                            ))}
-                                        </tbody>
-                                    </table>
-                                </Grid>
-                                <Grid item xs={1} />
-                                <Grid item xs={1} />
-                                <Grid item xs={10}>
-                                    <Typography align="center" variant="h6">
-                                        {`Total Nett Value Of Consignment (${
-                                            c.consignment.currencyCode
-                                        }): ${c.consignment.totalNettValue.toFixed(2)}`}
-                                    </Typography>
-                                </Grid>
-                                <Grid item xs={1} />
-                            </>
-                        );
-                    })}
-                </Grid>
+                                        ))}
+                                    </tbody>
+                                </table>
+                            </Grid>
+                            <Grid item xs={1} />
+                            <Grid item xs={1} />
+                            <Grid item xs={10}>
+                                <Typography align="center" variant="h6">
+                                    {`Total Nett Value Of Consignment (${
+                                        c.consignment.currencyCode
+                                    }): ${c.consignment.totalNettValue.toFixed(2)}`}
+                                </Typography>
+                            </Grid>
+                            <Grid item xs={1} />
+                        </Grid>
+                    );
+                })}
             </div>
         );
     }
