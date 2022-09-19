03fa27013f1ba71071f238695abe62acc2789efa
Richard Phillips <richard.phillips@linn.co.uk>
2022-09-19 15:31:25 +0100

    small instead of landscape

--- src/Service.Host/client/src/components/reports/WwdReport.js
+++ src/Service.Host/client/src/components/reports/WwdReport.js
@@ -3,48 +3,45 @@ import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import { ReportTable, Loading, ErrorCard, BackButton } from '@linn-it/linn-form-components-library';
 import PropTypes from 'prop-types';
-import Page from '../../containers/Page';
 
 export default function WwdReport({ reportData, loading, error, options, history }) {
     return (
-        <Page width="xl">
-            <div className="print-landscape pageContainer">
-                <Grid container spacing={3}>
-                    {error && (
-                        <Grid item xs={12}>
-                            <ErrorCard errorMessage={error} />
-                        </Grid>
-                    )}
-                    <Grid item xs={10}>
-                        <Typography variant="subtitle2">{`What Will Decrement Report - Part Number: ${
-                            options.partNumber
-                        }, Quantity: ${options.quantity}, Type of Run: ${options.typeOfRun}${
-                            options.workStationCode ? options.workStationCode : ''
-                        }`}</Typography>
-                    </Grid>
-                    <Grid item xs={2}>
-                        <BackButton
-                            backClick={() => history.push('/inventory/reports/what-will-decrement')}
-                        />
-                    </Grid>
+        <div className="zoomed-out-printing no-page">
+            <Grid container spacing={3}>
+                {error && (
                     <Grid item xs={12}>
-                        {loading ? (
-                            <Loading />
-                        ) : (
-                            reportData && (
-                                <ReportTable
-                                    reportData={reportData}
-                                    showTotals={false}
-                                    showTitle
-                                    title={reportData ? reportData?.title.displayString : 'Loading'}
-                                    showRowTitles
-                                />
-                            )
-                        )}
+                        <ErrorCard errorMessage={error} />
                     </Grid>
+                )}
+                <Grid item xs={10}>
+                    <Typography variant="subtitle2">{`What Will Decrement Report - Part Number: ${
+                        options.partNumber
+                    }, Quantity: ${options.quantity}, Type of Run: ${options.typeOfRun}${
+                        options.workStationCode ? options.workStationCode : ''
+                    }`}</Typography>
                 </Grid>
-            </div>
-        </Page>
+                <Grid item xs={2} className="hide-when-printing">
+                    <BackButton
+                        backClick={() => history.push('/inventory/reports/what-will-decrement')}
+                    />
+                </Grid>
+                <Grid item xs={12}>
+                    {loading ? (
+                        <Loading />
+                    ) : (
+                        reportData && (
+                            <ReportTable
+                                reportData={reportData}
+                                showTotals={false}
+                                showTitle
+                                title={reportData ? reportData?.title.displayString : 'Loading'}
+                                showRowTitles
+                            />
+                        )
+                    )}
+                </Grid>
+            </Grid>
+        </div>
     );
 }
 
