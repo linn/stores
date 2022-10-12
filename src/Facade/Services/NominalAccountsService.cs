0b9656836e9881d5350d6d65d039112e0cb7fbef
Lewis Renfrew <lewis.renfrew@linn.co.uk>
2022-10-12 12:09:37 +0100

    limit nominals properly as opposed to just take(50)

--- src/Facade/Services/NominalAccountsService.cs
+++ src/Facade/Services/NominalAccountsService.cs
@@ -24,10 +24,11 @@
                 .FilterBy(n
                     => !n.Department.DateClosed.HasValue && !n.Nominal.DateClosed.HasValue &&
                        n.Department.ObseleteInStores == "N" &&
+                       n.StoresPostsAllowed == "Y" &&
                        (n.Department.Description.ToUpper().Equals(searchTerm.ToUpper())
                         || n.Department.DepartmentCode.ToUpper().Equals(searchTerm.ToUpper())
                         || n.Nominal.Description.ToUpper().Equals(searchTerm.ToUpper())
-                        || n.Nominal.NominalCode.ToUpper().Equals(searchTerm.ToUpper()))).Take(50);
+                        || n.Nominal.NominalCode.ToUpper().Equals(searchTerm.ToUpper())));
             if (exactMatches.Any())
             {
                 return new SuccessResult<IEnumerable<NominalAccount>>(exactMatches);
