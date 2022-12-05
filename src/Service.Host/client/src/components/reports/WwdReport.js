fd48bc044869bb904fa14730e1bec88290599845
Richard Phillips <richard.phillips@linn.co.uk>
2022-12-05 16:01:44 +0000

    add incubated ReportTable

--- src/Service.Host/client/src/components/reports/WwdReport.js
+++ src/Service.Host/client/src/components/reports/WwdReport.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-import { ReportTable, Loading, ErrorCard, BackButton } from '@linn-it/linn-form-components-library';
+import { Loading, ErrorCard, BackButton } from '@linn-it/linn-form-components-library';
 import PropTypes from 'prop-types';
+import ReportTable from './ReportTable';
 
 export default function WwdReport({ reportData, loading, error, options, history }) {
     return (
