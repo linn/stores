75901bed3545c00cb3db28bc909d0cc4e3da2790
Richard Phillips <richard.phillips@linn.co.uk>
2024-02-08 15:20:23 +0000

    get stores budget posting by budget id

---
+++ src/Domain.LinnApps/StoresBudgetPostingKey.cs
@@ -0,0 +1,9 @@
+﻿namespace Linn.Stores.Domain.LinnApps
+{
+    public class StoresBudgetPostingKey
+    {
+        public int BudgetId { get; set; }
+
+        public int Sequence { get; set; }
+    }
+}
