f3881348ea455aac72b00778913c98ba9724c039
Andrew Fraser <andrewfraser73@hotmail.com>
2024-03-19 14:15:57 +0000

    call new bartender template

---
+++ tests/Unit/Domain.LinnApps.Tests/LogisticsLabelServiceTests/WhenPrintingGeneralLabel.cs
@@ -0,0 +1,50 @@
+﻿namespace Linn.Stores.Domain.LinnApps.Tests.LogisticsLabelServiceTests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    using FluentAssertions;
+
+    using Linn.Stores.Domain.LinnApps.Models;
+
+    using NSubstitute;
+
+    using NUnit.Framework;
+
+    public class WhenPrintingGeneralLabel : ContextBase
+    {
+        private ProcessResult result;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.result = this.Sut.PrintGeneralLabel(
+                "line1",
+                "line2",
+                "line3",
+                string.Empty,
+                string.Empty,
+                this.UserNumber);
+        }
+
+        [Test]
+        public void ShouldPrintLabel()
+        {
+            this.BartenderLabelPack.Received().PrintLabels(
+                $"GeneralLabel{23}",
+                "DispatchLabels1",
+                1,
+                "dispatchgeneral.btw",
+                Arg.Any<string>(),
+                ref Arg.Any<string>());
+        }
+
+        [Test]
+        public void ShouldReturnResult()
+        {
+            this.result.Message.Should().Be("1 general label(s) printed");
+            this.result.Success.Should().BeTrue();
+        }
+    }
+}
