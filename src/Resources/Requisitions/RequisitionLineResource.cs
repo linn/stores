8ed2785a1f5c4db37c24704ba8b51b6e5973c5e2
RossStewart <ross.stewart@linn.co.uk>
2024-06-11 12:06:11 +0100

    Loan Req fields (#496)
    * Loan Req fields
    
    * Resource etc
    
    * Unnecessary 'Using System'
    
    ---------
    
    Co-authored-by: rosss <rosss@PC1329>

--- src/Resources/Requisitions/RequisitionLineResource.cs
+++ src/Resources/Requisitions/RequisitionLineResource.cs
@@ -5,5 +5,13 @@
         public string TransactionCode { get; set; }
 
         public int Line { get; set; }
+
+        public string DateCancelled { get; set; }
+
+        public string CancelledReason { get; set; }
+
+        public int? CancelledBy { get; set; }
+
+        public int? Document1Line { get; set; }
     }
 }
