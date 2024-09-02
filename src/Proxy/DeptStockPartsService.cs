f302b83a5904717f8d137b20142ee217a45b7a0f
Lewis Renfrew <lewis.renfrew@linn.co.uk>
2024-09-02 13:42:31 +0100

    try faster query, possibly?

--- src/Proxy/DeptStockPartsService.cs
+++ src/Proxy/DeptStockPartsService.cs
@@ -19,21 +19,29 @@
         public IEnumerable<Part> GetDeptStockPalletParts()
         {
             var query =
-                @"SELECT PART.PART_NUMBER PART_NUMBER,
-                    PART.DESCRIPTION DESCRIPTION,
-                    PART.BRIDGE_ID BRIDGE_ID
-                    FROM PARTS PART 
-                    WHERE PART.PART_NUMBER IN
-                        (SELECT P.PART_NUMBER 
-                        FROM STOCK_LOCATORS SL,     
-                        PARTS P WHERE SL.PART_NUMBER = P.PART_NUMBER 
-                        and stock_pool_code in ('LINN DEPT','LS DEPT')) OR(PART.STOCK_CONTROLLED = 'N' 
-                        AND NVL(PART.BASE_UNIT_PRICE, 0) = 0 AND NVL(PART.LINN_PRODUCED, 'N') = 'N' 
-                        AND PART.PART_NUMBER NOT IN(SELECT P.PART_NUMBER FROM STOCK_LOCATORS SL, PARTS P WHERE SL.PART_NUMBER = P.PART_NUMBER))
-                    OR
-                        (exists(
-                            select 1 from stock_locators sl 
-                            where sl.part_number = part.part_number and sl.budget_id is null))";
+                @"SELECT PART.PART_NUMBER, PART.DESCRIPTION, PART.BRIDGE_ID
+                 FROM PARTS PART
+                WHERE EXISTS (
+                    SELECT 1
+                    FROM STOCK_LOCATORS SL
+                    WHERE SL.PART_NUMBER = PART.PART_NUMBER
+                      AND SL.STOCK_POOL_CODE IN ('LINN DEPT', 'LS DEPT')
+                )
+                OR (PART.STOCK_CONTROLLED = 'N'
+                    AND NVL(PART.BASE_UNIT_PRICE, 0) = 0
+                    AND NVL(PART.LINN_PRODUCED, 'N') = 'N'
+                    AND NOT EXISTS (
+                        SELECT 1
+                        FROM STOCK_LOCATORS SL
+                        WHERE SL.PART_NUMBER = PART.PART_NUMBER
+                    )
+                )
+                OR EXISTS (
+                    SELECT 1
+                    FROM STOCK_LOCATORS SL
+                    WHERE SL.PART_NUMBER = PART.PART_NUMBER
+                      AND SL.BUDGET_ID IS NULL
+                )";
 
             var rows = this.databaseService.ExecuteQuery(query).Tables[0].Rows;
             var result = new List<Part>();
