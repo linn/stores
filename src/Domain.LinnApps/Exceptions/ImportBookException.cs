78d695f485acbdf5e27e032a4280c39855d450f6
lewisrenfrew <Lewis.Renfrew@linn.co.uk>
2025-01-13 09:54:07 +0000

    Impbook bugs (#515)
    * stop impbook lines having rsn order number
    
    * belts & braces - stop problem happening from client too
    
    * test name

---
+++ src/Domain.LinnApps/Exceptions/ImportBookException.cs
@@ -0,0 +1,19 @@
+﻿namespace Linn.Stores.Domain.LinnApps.Exceptions
+{
+    using System;
+
+    using Linn.Common.Domain.Exceptions;
+
+    public class ImportBookException : DomainException
+    {
+        public ImportBookException(string message)
+            : base(message)
+        {
+        }
+
+        public ImportBookException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
