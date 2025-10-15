2418e6561af798507d41419fa2916ce7d5aaf5bb
Ross Stewart <ross.stewart@linn.co.uk>
2025-10-15 13:51:36 +0100

    response processor

---
+++ src/Service/ResponseProcessors/PrintResultResponseProcessor.cs
@@ -0,0 +1,16 @@
+﻿namespace Linn.Stores.Service.ResponseProcessors
+{
+    using System.Collections.Generic;
+
+    using Linn.Common.Facade;
+    using Linn.Common.Nancy.Facade;
+    using Linn.Stores.Domain.LinnApps;
+
+    public class PrintResultResponseProcessor : JsonResponseProcessor<PrintResult>
+    {
+        public PrintResultResponseProcessor(IResourceBuilder<PrintResult> resourceBuilder)
+            : base(resourceBuilder, "print-result", 1)
+        {
+        }
+    }
+}
