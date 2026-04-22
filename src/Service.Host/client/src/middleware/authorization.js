becd1900ff70d25322ae1fe094f58d3d2e0f910d
lewisrenfrew <Lewis.Renfrew@linn.co.uk>
2026-04-22 14:24:16 +0100

    redundant code (#565)
    * fix search
    
    * delete redundant auth middleware bypass
    
    * blank
    
    ---------
    
    Co-authored-by: Lewis Renfrew <LewisRe@linn.co.uk>

--- src/Service.Host/client/src/middleware/authorization.js
+++ src/Service.Host/client/src/middleware/authorization.js
@@ -1,16 +1,9 @@
 ﻿import { RSAA } from 'redux-api-middleware';
 import { getAccessToken } from '../selectors/getAccessToken';
-import { addressesActionTypes } from '../actions';
 
 export default ({ getState }) => next => action => {
     if (action[RSAA]) {
-        if (
-            action[RSAA].options &&
-            action[RSAA].options.requiresAuth &&
-            !action[RSAA]?.types?.some(
-                t => t.type === addressesActionTypes.REQUEST_SEARCH_ADDRESSES
-            )
-        ) {
+        if (action[RSAA].options && action[RSAA].options.requiresAuth) {
             // eslint-disable-next-line no-param-reassign
             action[RSAA].headers = {
                 Authorization: `Bearer ${getAccessToken(getState())}`,
@@ -18,6 +11,5 @@ export default ({ getState }) => next => action => {
             };
         }
     }
-
     return next(action);
 };
